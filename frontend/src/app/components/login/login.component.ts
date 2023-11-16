5a7c050773fe7f489b9406c70e72a5eec0cafdaa
Ricky Galpo <rickygalpo7777@gmail.com>
2023-11-16 09:59:22 +0800

    integrated loginApi to frontend

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Login } from 'src/app/interfaces/login';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  })
 
+  constructor(
+    private _loginService: LoginService,
+  ) { }
+
+  onSubmit() {
+    this._loginService.authenticate(this.loginForm.value).subscribe(
+      (response) => {
+        console.log(response);
+    })
+  }
 }
